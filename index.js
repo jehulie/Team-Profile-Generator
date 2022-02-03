18e4e23ac73af264843d4ff3d59b675516fede10
Julie Chu <julieychu@gmail.com>
2022-02-03 17:53:53 -0600

    fix generateHTML file

--- index.js
+++ index.js
@@ -1,24 +1,112 @@
-const fs = require('fs');
+// const Prompt = require('./src/Prompt');
 const generateHTML = require('./src/generateHTML');
-const Prompt = require('./src/Prompt');
+const fs = require('fs');
+const path = require('path');
+
+const outputDir = path.resolve(__dirname, 'dist');
+const outputPath = path.join(outputDir, 'index.html');
+
+// Inquirer helper function
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const employeeArr = []
+
+function Prompt() {
+    return inquirer
+    .prompt([
+    {
+        type: 'list',
+        name: 'role',
+        message:"What is the employee's role?",
+        choices: ['Manager', 'Engineer', 'Intern']
+    },
+    {
+        type:'text',
+        name: 'name',
+        message: "What is the Employee's name?"
+    },
+    {
+        type:'text',
+        name: 'id',
+        message: "What is the employee's ID number?"
+    },
+    {
+        type: 'text',
+        name: 'email',
+        message: "What is the employee's email?"
+    }])
+    .then(({name, id, email, role}) => {
+        if (role === "Manager") {
+            return inquirer
+                .prompt([{
+                    type:'text',
+                    name: 'office',
+                    message:"What is the Manager's office number?"
+                },
+                {
+                    type:'confirm',
+                    name:'anotherEntry',
+                    message: "Would you like to add another employee?",
+                    default: false
+                }])
+                .then(({office, anotherEntry}) => {
+                    employeeArr.push(new Manager(name, id, email, office))
+                    console.log(employeeArr)
+                    if (anotherEntry) {
+                        return Prompt();
+                    }
+                })
+        } else if (role === "Engineer") {
+            return inquirer
+                .prompt([{
+                    type: 'text',
+                    name: 'github',
+                    message: "What is the Engineer's Github username?"
+                },
+                {
+                    type:'confirm',
+                    name:'anotherEntry',
+                    message: "What you like to add another employee?",
+                    default: false
+                }])
+                .then(({github, anotherEntry}) => {
+                    employeeArr.push(new Engineer(name, id, email, github))
+                    console.log(employeeArr)
+                    if (anotherEntry) {
+                        return Prompt();
+                    }
+                })
+        } else if (role === 'Intern') {
+             return inquirer
+                .prompt([{
+                    type:'text',
+                    name:'school',
+                    message: "What is the Intern's school?"
+                },
+                {
+                    type:'confirm',
+                    name:'anotherEntry',
+                    message: "What you like to add another employee?",
+                    default: false
+                }])
+                .then(({school, anotherEntry}) => {
+                    employeeArr.push(new Intern(name, id, email, school))
+                    console.log(employeeArr)
+                    if (anotherEntry) {
+                        return Prompt();
+                    }
+                })
+        }
+    })
+};
 
 Prompt()
-  .then((employeeArr) => {
-    const htmlPageContent = generateHTML(employeeArr);
+  .then(() => {
 
-    fs.writeFile('./dist/index.html', htmlPageContent, (err) =>
+    fs.writeFile(outputPath, generateHTML(employeeArr), (err) =>
       err ? console.log(err) : console.log('Successfully created index.html!')
     );
   });
-
-//  Prompt()
-    // .then(employeeArr => {
-    //     return generateHTML(employeeArr)
-    // })
-    // .then(pageHTML => {
-    //     return writeFile(pageHTML)
-    // })
-
-    // .catch(err => {
-    //     console.log(err);
-    // });
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,56 +1,63 @@
-const Manager = require('../lib/Manager');
-const Engineer = require('../lib/Engineer')
-const Intern = require('../lib/Intern')
-
 // A function to generate team member cards
-const generateCards = employeeArr => {
- 
-    const manager = employeeArr.manager.map(function(job) {
+function generateCards(employeeArr) {
+
+    function managerCard(manager) {
         let managerHtml = `
         <div class="card" style="width: 18rem;">
-            <h2>${job.name}</h2>
+            <h2>${manager.getName()}</h2>
             <h4>Manager<h4>
-            <p>ID: ${job.id}</p>
-            <p>Email: <a href="mailto:${job.email}">${job.email}</a></p>
-            <p>Office Number: ${job.office}</p>
+            <p>ID: ${manager.getId()}</p>
+            <p>Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></p>
+            <p>Office Number: ${manager.getofficeNumber()}</p>
         </div>
         `
         return managerHtml
-    });
+    };
 
-    const engineer = employeeArr.engineer.map(function(job) {
+    function engineerCard(engineer) {
         let engineerHtml = `
         <div class="card" style="width: 18rem;">
-            <h2>${job.name}</h2>
+            <h2>${engineer.name}</h2>
             <h4>Engineer<h4>
-            <p>ID: ${job.id}</p>
-            <p>Email: <a href="mailto:${job.email}">${job.email}</a></p>
-            <p> Github: <a href="https://github.com/${job.github}" target="_blank">${job.github}</a></p>
+            <p>ID: ${engineer.id}</p>
+            <p>Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+            <p> Github: <a href="https://github.com/${engineer.github}" target="_blank">${engineer.github}</a></p>
         </div>
         `
         return engineerHtml
-    })
+    };
 
-    const intern = employeeArr.intern.map(function(job) {
+    function internCard(intern) {
         let interHtml = `
         <div class="card" style="width: 18rem;">
-            <h2>${job.name}</h2>
+            <h2>${intern.name}</h2>
             <h4>Intern<h4>
-            <p>ID: ${job.id}</p>
-            <p>Email: <a href="mailto:${job.email}">${job.email}</a></p>
-            <p> School: ${job.school}</p>
+            <p>ID: ${intern.id}</p>
+            <p>Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+            <p> School: ${intern.school}</p>
         </div>
         `
         return interHtml
-    })
-    return [manager,engineer,intern]
+    };
+    const htmlArray = [];
+
+    htmlArray.push(employeeArr.filter(employee=>employee.getRole()==='Manager')
+    .map(manager=>managerCard(manager)).join(''));
+
+    htmlArray.push(employeeArr.filter(employee=>employee.getRole()==='Engineer')
+    .map(manager=>engineerCard(manager)).join(''));
+
+    htmlArray.push(employeeArr.filter(employee=>employee.getRole()==='Intern')
+    .map(manager=>internCard(manager)).join(''));
+
+    return htmlArray.join('')
 }
 
 // A function to generate HTML content
 
-function generateHTML (employeeArr) {
-    return
-  `<!DOCTYPE html>
+module.exports = employeeArr => {
+    return`
+    <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -71,4 +78,3 @@ ${generateCards(employeeArr)}
 </html>`
 };
 
-module.exports = generateHTML;
